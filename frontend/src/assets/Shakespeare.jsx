abfc03ceb76dc695bbdebb6b7c6ac541d1150f43
lucianogarciaz <luciano.garcia@typeform.com>
2023-04-01 11:53:30 -0300

    resize the shakespeare svg

--- frontend/src/assets/Shakespeare.jsx
+++ frontend/src/assets/Shakespeare.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function Shakespeare() {
-  const size = 50;
+  const size = 30;
   const radius = size / 2;
   return (
     <svg width={size} height={size}>
