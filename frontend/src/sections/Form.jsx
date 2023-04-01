4fc162da025f72d871620502aafb39b426df281b
lucianogarciaz <luciano.garcia@typeform.com>
2023-04-01 11:51:46 -0300

    re-organize app as a chat app

--- frontend/src/sections/Form.jsx
+++ frontend/src/sections/Form.jsx
@@ -1,27 +1,23 @@
 import React, { useState } from 'react';
-import Filter from './Filter';
 
 export default function Form() {
   const [inputValue, setInputValue] = useState('');
 
-  const handleSelectedOption = (options) => {
-    const value = options.label;
-    setInputValue(value);
-  };
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
 
   return (
     <form className="search">
-      <Filter onSelectedOptionsChange={handleSelectedOption} />
       <textarea
         className="box"
         value={inputValue}
         onChange={handleInputChange}
         placeholder="Chat with Shakespeare: Ask about his life, works, or characters..."
       />
-      <button type="submit">Discover</button>
+      <div className="circle">
+        <button type="submit">Ask</button>
+      </div>
     </form>
   );
 }
