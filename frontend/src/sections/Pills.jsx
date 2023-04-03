9b1f7f89a8f2555d490636872b8a2676fb5443c1
lucianogarciaz <luciano.garcia@typeform.com>
2023-04-03 18:00:34 -0300

    add pull of questions

--- frontend/src/sections/Pills.jsx
+++ frontend/src/sections/Pills.jsx
@@ -1,29 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 export default function Pills({ handlePillClick }) {
-  const firstSuggestion = "What prophecy drives Macbeth's ambition?";
-  const secondSuggestion = "Who is Prospero's servant in 'The Tempest?";
-  const thirdSuggestion = 'Who betrays Julius Caesar in his play?';
-  const fourthSuggestion = "Who are Romeo and Juliet's feuding families?";
+  const [suggestions, setSuggestions] = useState([]);
+
+  const questions = [
+    "What prophecy drives Macbeth's ambition?",
+    "Who is Prospero's servant in 'The Tempest?",
+    'Who betrays Julius Caesar in his play?',
+    "Who are Romeo and Juliet's feuding families?",
+    "Who disguises as a lawyer in 'Merchant of Venice'?",
+    'Which play features fairy queen Titania?',
+    "What forest is 'As You Like It' set in?",
+    'Which play has a jealous King Leontes?',
+    "Who is the king of Britain in 'King Lear'?",
+    'What ghost haunts Hamlet in his play?',
+    "Which play features the 'Three Witches'?",
+    "Who is Othello's manipulative ensign?",
+    "What island is 'The Tempest' set on?",
+    'Which comedy has twins Viola and Sebastian?',
+    "Who speaks the monologue 'All the world's a stage'?",
+    "Which play has the famous line 'To be or not to be'?",
+    'What play features the mischievous character Puck?',
+    "Who is the Moorish general in 'Othello'?",
+    "Which Shakespeare play is also known as 'The Scottish Play'?",
+    "What is the name of the fairy king in 'A Midsummer Night's Dream'?",
+    "Who is the Prince of Denmark in 'Hamlet'?",
+    'Which play has the character of the Duke of Vienna disguised as a friar?',
+    "In which play does a character say 'Parting is such sweet sorrow'?",
+    "Who is the daughter of the Duke of Milan in 'The Tempest'?",
+    'Which play has the famous balcony scene?',
+    "What is the name of the town in 'Much Ado About Nothing'?",
+    'Which play features the character Shylock, a Jewish moneylender?',
+    'In which play does the Queen of Egypt fall in love with a Roman general?',
+    "What is the name of the King of England in 'Richard III'?",
+    'Which play features the Battle of Agincourt?',
+  ];
+
+  useEffect(() => {
+    const randomQuestions = [];
+    const questionsCopy = [...questions];
+
+    for (let i = 0; i < 4; i += 1) {
+      const randomIndex = Math.floor(Math.random() * questionsCopy.length);
+      randomQuestions.push(questionsCopy[randomIndex]);
+      questionsCopy.splice(randomIndex, 1);
+    }
+
+    setSuggestions(randomQuestions);
+  }, []);
 
   return (
     <>
       <div className="pills">
-        <button type="button" className="pill" onClick={() => handlePillClick(firstSuggestion)}>
-          {firstSuggestion}
-        </button>
-        <button type="button" className="pill" onClick={() => handlePillClick(secondSuggestion)}>
-          {secondSuggestion}
-        </button>
+        {suggestions.slice(0, 2).map((suggestion) => (
+          <button
+            key={suggestion}
+            type="button"
+            className="pill"
+            onClick={() => handlePillClick(suggestion)}
+          >
+            {suggestion}
+          </button>
+        ))}
       </div>
       <div className="pills">
-        <button type="button" className="pill" onClick={() => handlePillClick(thirdSuggestion)}>
-          {thirdSuggestion}
-        </button>
-        <button type="button" className="pill" onClick={() => handlePillClick(fourthSuggestion)}>
-          {fourthSuggestion}
-        </button>
+        {suggestions.slice(2).map((suggestion) => (
+          <button
+            key={suggestion}
+            type="button"
+            className="pill"
+            onClick={() => handlePillClick(suggestion)}
+          >
+            {suggestion}
+          </button>
+        ))}
       </div>
     </>
   );
