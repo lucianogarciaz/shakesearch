0747b703b328d45cc94e1931075e2fea6f03d59f
lucianogarciaz <luciano.garcia@typeform.com>
2023-04-02 22:24:34 -0300

    add more pills and questions

--- frontend/src/sections/Pills.jsx
+++ frontend/src/sections/Pills.jsx
@@ -2,18 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Pills({ handlePillClick }) {
-  const firstSuggestion = 'Can you tell us more about your creative process?';
-  const secondSuggestion = 'Did you enjoy writing comedies more?';
+  const firstSuggestion = "What prophecy drives Macbeth's ambition?";
+  const secondSuggestion = "Who is Prospero's servant in 'The Tempest?";
+  const thirdSuggestion = 'Who betrays Julius Caesar in his play?';
+  const fourthSuggestion = "Who are 'Romeo and Juliet's' feuding families?";
 
   return (
-    <div className="pills">
-      <button type="button" className="pill" onClick={() => handlePillClick(firstSuggestion)}>
-        {firstSuggestion}
-      </button>
-      <button type="button" className="pill" onClick={() => handlePillClick(secondSuggestion)}>
-        {secondSuggestion}
-      </button>
-    </div>
+    <>
+      <div className="pills">
+        <button type="button" className="pill" onClick={() => handlePillClick(firstSuggestion)}>
+          {firstSuggestion}
+        </button>
+        <button type="button" className="pill" onClick={() => handlePillClick(secondSuggestion)}>
+          {secondSuggestion}
+        </button>
+      </div>
+      <div className="pills">
+        <button type="button" className="pill" onClick={() => handlePillClick(thirdSuggestion)}>
+          {thirdSuggestion}
+        </button>
+        <button type="button" className="pill" onClick={() => handlePillClick(fourthSuggestion)}>
+          {fourthSuggestion}
+        </button>
+      </div>
+    </>
   );
 }
 
